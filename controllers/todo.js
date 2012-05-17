83c8ea9f592485992c042d0f45adfcb967730424
George <shankga@gmail.com>
2012-05-16 21:33:50 -0600

    Yup

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,11 +1,20 @@
-function TodoCtrl($scope) {
+angular.module('app', ['ngResource']).
+  config(function($routeProvider){
+    $routeProvider.
+      when('/list', {controller:TodoCtrl, template: 'views/list.html'}).
+      when('/show/:id', {controller:TodoCtrl, template: 'views/show.html'}).
+      otherwise({redirectTo:'/list'});
+  });
+
+function TodoCtrl($scope, $routeParams) {
   $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
- 
+    {text:'learn angular', done:true, id:0},
+    {text:'build an angular app', done:false, id:0}];
+ $scope.showtodo = $scope.todos[$routeParams.id];
   $scope.addTodo = function() {
     $scope.todos.push({text:$scope.todoText, done:false});
     $scope.todoText = '';
+    assignIds($scope);
   };
  
   $scope.remaining = function() {
@@ -22,5 +31,14 @@ function TodoCtrl($scope) {
     angular.forEach(oldTodos, function(todo) {
       if (!todo.done) $scope.todos.push(todo);
     });
+    assignIds($scope);
   };
+}
+
+//Helpers
+
+function assignIds ($scope){
+  for(var i =0; i< $scope.todos.length; ++i){
+    $scope.todos[i].id = i;
+  }
 }
